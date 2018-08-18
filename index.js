d40e9745eb12f4744977b18b3893bf5f057df855
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-08-18 17:36:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ function increaseRankBy(n) {
         children[j].innerHTML = parseInt(children[j].innerHTML)+n
       }
 
-      function deepestChild() {
-        let node = document.getElementById('grand-node')
-        let childNode = node.children[0]
-         while(childNode) {
-           node = childNode
-           childNode = node.children[0];
-         }
-          return node;
-      }
+function deepestChild(){
+  let node = document.getElementById('grand-node')
+  let childNode = node.children[0]
+    while(childNode) {
+      node = childNode
+      childNode = node.children[0];
    }
+  return node;
+  }
+ }
 }
