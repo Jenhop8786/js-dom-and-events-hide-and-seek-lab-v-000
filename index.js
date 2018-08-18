05bbca57210137d97522d2506b628d0df31bb448
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-08-18 17:29:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,13 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   return document.querySelector("div.target");
 }
+
+function increaseRankBy(n) {
+  const rankedList = document.querySelectorAll('.ranked-list')
+   for (let i = 0, length = rankedList.length; i < length; i++) {
+     let children = rankedList[i].children;
+      for (let l = 0, j = children.length; l < j; l++) {
+        children[j].innerHTML = parseInt(children[l].innerHTML)+n
+      }
+   }
+}
